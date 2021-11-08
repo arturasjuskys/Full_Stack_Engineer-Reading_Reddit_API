6ebbf66778c8133c273fe3808771d29fed085a01
AJuskys <81487998+AJuskys@users.noreply.github.com>
2021-11-08 17:07:42 +0000

    Adding - Displaying Videos

--- src/components/FullArticle.js
+++ src/components/FullArticle.js
@@ -1,10 +1,7 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
 import ReactMarkdown from 'react-markdown';
-// import 
-import {
-  selectAllArticles
-} from '../features/articlePreviews/articlePreviewsSlice'
+import { selectAllArticles } from '../features/articlePreviews/articlePreviewsSlice'
 
 export default function FullArticle() {
   const articles = useSelector(selectAllArticles);
@@ -16,6 +13,7 @@ export default function FullArticle() {
     return article;
   };
   const article = getArticle();
+  console.log(article);
 
   const getImg = () => {
     const img = article.url;
@@ -26,6 +24,23 @@ export default function FullArticle() {
       return;
     }
   };
+
+  const getVideo = () => {
+    if (!article.media) return;
+    const video = article.media.reddit_video.fallback_url;
+    const height = article.media.reddit_video.height;
+    const width = article.media.reddit_video.width;
+    const result = {
+      url: video,
+      height,
+      width,
+    };
+    console.log(result);
+    return <video width={width} height={height} controls>
+      <source src={video} type="video/mp4" />
+    </video>
+    };
+  getVideo();
 
   return (
     <article className="full-article">
@@ -40,14 +55,12 @@ export default function FullArticle() {
           <h2 className="full-title">{article.title}</h2>
         </div>
         {getImg()}
+        {getVideo()}
         <ReactMarkdown>{article.selftext}</ReactMarkdown>
         <div className="info-comments">
           <img className="comments-icon" src="/img/comment.png" alt="comments icon" />
           <p className="comments">2.4k comments</p>
           <a className="open-reddit" rel="noreferrer" target="_blank" href={`http://reddit.com/${article.permalink}`}>Open in Reddit</a>
-        </div>
-        <div className="loaded-comments">
-
         </div>
       </div>
     </article>
