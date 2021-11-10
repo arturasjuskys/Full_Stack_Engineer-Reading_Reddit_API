d99a857e317004066789ad938d9e787c2426144d
AJuskys <81487998+AJuskys@users.noreply.github.com>
2021-11-10 12:17:27 +0000

    Deleting - Aside Store IDs

--- src/app/Main.js
+++ src/app/Main.js
@@ -1,12 +1,11 @@
 import React from "react";
 import { Switch, Route } from 'react-router-dom';
-import ArticlePreviews from "../features/articles/Article";
+import ArticlePreviews from "../features/articles/Articles";
 import FullArticle from '../components/FullArticle';
 
 export default function Main () {
   return (
     <main className="full-articles">
-      {/* <h2>Posts</h2> */}
       <Switch>
         <Route exact path="/">
           <ArticlePreviews />
